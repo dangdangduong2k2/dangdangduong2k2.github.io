b1df261a2175b13b0a0dcdacbcd47b66577bfd55
dangdangduong2k2 <dangdangduong2k2@gmail.com>
2025-02-17 02:04:26 +0700

    Fix directory structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,8 +55,5 @@ navLinks.forEach(link => {
 // Ensure no section is highlighted when the page is loaded and scrolled to the top
 window.addEventListener('load', () => {
     navLinks.forEach(link => link.classList.remove('active'));
-<<<<<<< HEAD
-=======
-    highlightActiveSection(); // Call this to ensure the correct section is highlighted after removing all active classes
->>>>>>> parent of 5ac090b (Fix directory structure)
+    
 });
