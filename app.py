4eebfe2510e2e813139da5575a2339431788fe34
dangdangduong2k2 <botbot1232@gmail.com>
2025-05-23 23:12:42 +0700

    add popup detail

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__, template_folder='.', static_folder='assets')
 def home():
     return render_template('index.html')
 
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+# Không cần thay đổi gì ở đây nếu đã để popup.html ở static hoặc assets
+
 if __name__ == '__main__':
     app.run(debug=True)
